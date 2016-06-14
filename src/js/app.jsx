8d3a5f8d51dc045239c16c07d0bd3bd2ac89a998
Michael Tauer <michael.tauer@futurice.com>
2016-06-14 20:34:02 +0200

    Create Redux store

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import rootReducer from './reducers/rootReducer'
 
 function initApp() {
-  ReactDOM.render(<p>Hello world from ReactJS</p>, document.getElementById('react-root'))
+  const store = createStore( rootReducer, undefined, enableReduxDevToolsExtension())
+
+  ReactDOM.render(
+    <Provider store={store}>
+      <p>Hello world from ReactJS</p>
+    </Provider>,
+    document.getElementById('react-root')
+  )
+}
+
+function enableReduxDevToolsExtension() {
+  return (typeof window === 'object' && typeof window.devToolsExtension !== 'undefined'
+    ? window.devToolsExtension()
+    : f => f)
 }
 
 initApp()
