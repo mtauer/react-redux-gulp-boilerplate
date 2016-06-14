0ed81687e73bc0a105b600d842ec9b6c2ef4d767
Michael Tauer <michael.tauer@futurice.com>
2016-06-14 19:34:17 +0200

    Update default tasks

--- gulp-tasks/default.js
+++ gulp-tasks/default.js
@@ -1,6 +1,6 @@
 import gulp from 'gulp'
 import gulpSequence from 'gulp-sequence'
 
-gulp.task('default', ['build'])
+gulp.task('default', ['watch'])
 gulp.task('build', gulpSequence('test', 'clean', ['styles:build', 'javascript:build', 'staticFiles:build']))
-gulp.task('watch', ['test', 'styles:watch', 'javascript:watch', 'staticFiles:watch'])
+gulp.task('watch', gulpSequence('test', ['styles:watch', 'javascript:watch', 'staticFiles:watch'], 'browser-sync'))
