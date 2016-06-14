5084919a778058018c207411dac5967e5f0b6c28
Michael Tauer <michael.tauer@futurice.com>
2016-06-14 21:07:36 +0200

    Increment tick with timer

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -4,9 +4,11 @@ import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import rootReducer from './reducers/rootReducer'
 import HelloWorld from './components/helloWorld.jsx'
+import { incrementTick } from './actions/actions'
 
 function initApp() {
   const store = createStore( rootReducer, undefined, enableReduxDevToolsExtension())
+  connectTimer(store)
 
   ReactDOM.render(
     <Provider store={store}>
@@ -20,6 +22,12 @@ function enableReduxDevToolsExtension() {
   return (typeof window === 'object' && typeof window.devToolsExtension !== 'undefined'
     ? window.devToolsExtension()
     : f => f)
+}
+
+function connectTimer(store) {
+  window.setInterval(() => {
+    store.dispatch(incrementTick())
+  }, 5000)
 }
 
 initApp()
